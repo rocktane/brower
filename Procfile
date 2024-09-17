7b19da4a5451d03dda13954afe4d8a31347fa7fa
Yohan Gouiran <y.gouiran@gmail.com>
2024-09-18 01:37:50 +0200

    bin linux

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: ./pocketbase/pocketbase serve --http 0.0.0.0:8090 & npm start
+web: ./pocketbase/bin/pocketbase serve --http 0.0.0.0:8090 & npm start
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import i18n from "./i18n";
-// import PocketBase from "pocketbase";
-
-// const pb = new PocketBase(process.env.VUE_APP_POCKETBASE_URL);
 
 const app = createApp(App);
 
